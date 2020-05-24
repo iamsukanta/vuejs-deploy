8b261898403f7f67866dec23ae83c359025ab5e1
sukantabala28 <sukantabalacste2016@gmail.com>
2020-05-24 11:01:54 +0600

    silly changes

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+
+//Pages
+import Home from "../components/HomeComponent.vue";
+import About from "../components/AboutComponent.vue";
+import Contact from "../components/ContactComponent.vue";
+
+
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  scrollBehavior: () => ({ y: 0 }),
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+      name: 'HomeRoot',
+      meta: {
+        auth: false
+      },
+      component: {
+        render(c) { return c('router-view') }
+      },
+      children: [
+        {
+          path: 'home',
+          name: 'Home',
+          component: Home
+        },
+        {
+          path: 'about',
+          name: 'About',
+          component: About
+        },
+        {
+          path: 'contact',
+          name: 'Contact',
+          component: Contact
+        }
+      ]
+    }
+  ]
+});
